d2caa6c5f11c1c5cb2dac905a93ddc1829dcaf3e
DanielaOrbes <daniiorbes15@gmail.com>
2024-01-17 13:51:06 -0300

    recibir y pasar props a un componente, personalizar img, texto en negrita

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+
 import '../stylesheets/Testimonio.css'
 
-function Testimonio() {
+function Testimonio(props) {
+
   return (
+
     <div className="box-testimonio">
+
       <img
         className="img-testimonio"
-        src={require("../img/emma.png")}
+        src={require(`../img/${props.img}.png`)}
         alt="Imagen de Emma"
       />
+
       <div className="box-text">
-        <p className="name-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniero de software en Spotify</p>
-        <p className="text-testimonio">"Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+
+        <p className="name-testimonio"><strong>{props.name}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
+        <p className="text-testimonio">"{props.testimonio}"</p>
+
       </div>
 
     </div>
